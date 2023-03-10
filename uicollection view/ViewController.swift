c7ff24de8e69ee4b4c3433a74c0a4a8c52698abe
Adlet <adlet.zh19966@gmail.com>
2023-03-10 10:32:11 +0600

    Initial Commit

---
+++ uicollection view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uicollection view
+//
+//  Created by Adlet Zhantassov on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
